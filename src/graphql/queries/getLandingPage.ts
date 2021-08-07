6d50a49c2c8c60e2bd0825c7ca79af20a0871a12
Ana Helena <anahelenarp@hotmail.com>
2021-08-07 19:02:30 -0300

    Add header

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,30 @@
 const GET_LANDING_PAGE = /* GraphQL */ `
-  query GET_LANDING_PAGE {
-    landingPage {
-      logo {
-        alternativeText
+  fragment logo on LandingPage {
+    logo {
+      alternativeText
+      url
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
         url
       }
+      image {
+        url
+        alternativeText
+      }
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
     }
   }
 `
