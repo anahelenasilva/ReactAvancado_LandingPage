9f11ddfd55be7ecefed0e250b9fd503684d186cf
Ana Helena <anahelenarp@hotmail.com>
2021-08-08 10:08:35 -0300

    Section about us

--- src/types/api.ts
+++ src/types/api.ts
@@ -22,6 +22,24 @@ export type Button = {
   url: string
 }
 
+export type Author = {
+  photo: Image
+  name: string
+  role: string
+  socialLinks: SocialLink[]
+  description: string
+}
+
+export type SocialLink = {
+  title: string
+  url: string
+}
+
+export type Image = {
+  alternativeText: string
+  url: string
+}
+
 export type LogoProps = {
   alternativeText: string
   url: string
@@ -74,6 +92,11 @@ export type PricingBoxProps = {
   button: Button
 }
 
+export type SectionAboutUsProps = {
+  title: string
+  authors: Author[]
+}
+
 export type LandingPageProps = {
   logo: LogoProps
   header: HeaderProps
@@ -83,4 +106,5 @@ export type LandingPageProps = {
   sectionModules: SectionModulesProps
   sectionAgenda: SectionAgendaProps
   pricingBox: PricingBoxProps
+  sectionAboutUs: SectionAboutUsProps
 }
